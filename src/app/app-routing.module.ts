c737843ead7def4fec7cc2690c06fef038bdc698
RPHertiansa <reyhertiansa@gmail.com>
2020-12-12 00:02:35 +0700

    create components, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductCardComponent } from './product-card/product-card.component'
+import { FoodDetailComponent } from './food-detail/food-detail.component'
+import { CartComponent } from './cart/cart.component'
+import { SuccessComponent } from './success/success.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'foods',
+    component: ProductCardComponent
+  },
+  {
+    path: 'foods/:id',
+    component: FoodDetailComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'success',
+    component: SuccessComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { CartComponent } from './cart/cart.component';
+import { FoodDetailComponent } from './food-detail/food-detail.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    CartComponent,
+    FoodDetailComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
